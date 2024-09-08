29050432ca5b7f72c13ec3a637d204b7ee0433ba
nouman66 <noumanb60@gmail.com>
2024-09-09 04:33:12 +0500

    Fixing formatting issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 
 module "vpc" {
-  source                                 = "./modules/vpc"
-  vpc_name                               = var.vpc_name
-  aws_region                             = var.aws_region
-  vpc_cidr_block                         = var.vpc_cidr_block
-  vpc_public_subnets                     = var.vpc_public_subnets
-  vpc_private_subnets                    = var.vpc_private_subnets
-  vpc_protected_subnets                  = var.vpc_protected_subnets
-  common_tags                            = local.common_tags
-  eks_cluster_name                       = local.eks_cluster_name
+  source                = "./modules/vpc"
+  vpc_name              = var.vpc_name
+  aws_region            = var.aws_region
+  vpc_cidr_block        = var.vpc_cidr_block
+  vpc_public_subnets    = var.vpc_public_subnets
+  vpc_private_subnets   = var.vpc_private_subnets
+  vpc_protected_subnets = var.vpc_protected_subnets
+  common_tags           = local.common_tags
+  eks_cluster_name      = local.eks_cluster_name
 }
 
 module "bastion_host" {
